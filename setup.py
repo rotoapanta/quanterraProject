c050b4451a4c580cec3e57aefa705a7ed7d8eb1d
Roberto Toapanta <rtoapanta@igepn.edu.ec>
2023-08-10 14:38:39 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='quanterraProject',
+    version='1.2',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='rtoapanta',
+    author_email='robertocarlos.toapantagmail.com',
+    description='Quanterra Project'
+)
